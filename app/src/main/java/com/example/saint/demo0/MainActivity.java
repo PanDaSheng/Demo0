1cbfb5dfff1b85a06c541adc99e0da4b8e451375
Dasheng Pan <164116985@qq.com>
2017-02-18 14:43:27 +0800

    review code后，发现可以优化的地方，优化了代码。 1，button触发的事件相同，只是文字不同，所以把五个button用switch合并起来,看起来比较简洁。 2，优化了toast消息连续显示的一个小问题。

--- app/src/main/java/com/example/saint/demo0/MainActivity.java
+++ app/src/main/java/com/example/saint/demo0/MainActivity.java
@@ -2,70 +2,50 @@ package com.example.saint.demo0;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button bt1, bt2, bt3, bt4, bt5;//声明
+    private static Toast mToast = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //初始化
-        bt1 = (Button) findViewById(R.id.button);
-        bt2 = (Button) findViewById(R.id.button2);
-        bt3 = (Button) findViewById(R.id.button3);
-        bt4 = (Button) findViewById(R.id.button4);
-        bt5 = (Button) findViewById(R.id.button5);
-        //注册点击事件,内部匿名类，用toast显示通知
-        bt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "各种电影", Toast.LENGTH_LONG);
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.show();
-            }
-        });
-        bt2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "各种股票", Toast.LENGTH_LONG);
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.show();
-            }
-        });
-        bt3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "有很多书籍", Toast.LENGTH_LONG);
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.show();
-            }
-        });
-        bt4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "第一时间播报", Toast.LENGTH_LONG);
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.show();
-            }
-        });
-        bt5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "完整论文", Toast.LENGTH_LONG);
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.show();
-            }
-        });
-
     }
+
+    //既然每个按钮都触发一个相同的方法，就优化下把它们用switch合并起来。
+    //activitymaitn 那的button 改用  android:OnClick="btnClick";
+    public void btnClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                showToast(R.string.btn_msg1);
+                break;
+            case R.id.button2:
+                showToast(R.string.btn_msg2);
+                break;
+            case R.id.button3:
+                showToast(R.string.btn_msg3);
+                break;
+            case R.id.button4:
+                showToast(R.string.btn_msg4);
+                break;
+            case R.id.button5:
+                showToast(R.string.btn_msg5);
+                break;
+        }
+    }
+
+    //toast具体方法
+    private void showToast(int resId) {
+        if (mToast == null) {
+            mToast = Toast.makeText(this, resId, Toast.LENGTH_SHORT);
+        } else {
+            mToast.setText(resId);
+        }
+        mToast.show();
+    }
+
+
 }
